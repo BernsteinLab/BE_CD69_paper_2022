e99cec7ac4f7142fdd7c6227b32fc00395ae7e28
njaved <njaved@mit.edu>
2022-12-27 18:54:40 +0000

    ensure sonnet version 2.0.0

--- enformer_fine_tuning/tpu_pod_init_script.sh
+++ enformer_fine_tuning/tpu_pod_init_script.sh
@@ -4,7 +4,7 @@ sudo pip3 install silence_tensorflow
 sudo pip3 install tensorflow-addons
 sudo pip3 install matplotlib==3.4.3
 sudo pip3 install pandas
-sudo pip3 install dm-sonnet
+sudo pip3 install dm-sonnet==2.0.0
 sudo pip3 install tensorflow-probability
 sudo pip3 install seaborn
 sudo pip3 install einops
